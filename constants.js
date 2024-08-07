b4b7618276de004456847f3788ac08fb203d52d3
Balaji2404R <146181220+Balaji2404R@users.noreply.github.com>
2024-08-07 22:21:03 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,54 @@
+
+export const services = [
+    {
+        title: "Full Stack Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "UI&UX Designer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Balaji';
+
+export const experiences = [
+    {
+        'company': 'Pheonix-SoftTech',
+        'role': 'Programing in Java'
+       ,
+        'points': [
+            'Experience in object-oriented programming',
+            'Learn to develop and debug applications using IDEs like IntelliJ or Eclipse',
+            'Understand core Java concepts like classes, inheritance, and polymorphism' ,
+            'Work with frameworks like Spring for web development' ,
+            'Practice version control with Git for collaborative coding'
+        ],
+        
+    },
+    {
+        'company': 'Web Walk Infosysis',
+        'role': 'Big Data'
+        ,
+        
+        'points': [
+            ' I assisted in managing large datasets, performing data cleaning and preprocessing tasks',
+            'Contributed to the development of data-driven insights that supported business decision-making',
+            'This internship not only honed my technical skills but also deepened my understanding of how data can drive innovation and efficiency in modern organizations.'
+        ]
+
+
+
+        
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
